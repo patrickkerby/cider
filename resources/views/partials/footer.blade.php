39b50df81926f72f22e2256f6eae958a838c79b9
Patrick Kerby <patrick@kerbydesign.com>
2022-03-02 11:03:24 -0700

    responsive tweaks for footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -19,7 +19,7 @@
     <h4>Keep up with new products &amp; farm updates!</h4>
     <!-- Begin Mailchimp Signup Form -->
     <div id="mc_embed_signup">
-      <form action="" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+      <form action="https://prairiebearscider.us14.list-manage.com/subscribe/post?u=e6ab7586e5e3358172da22416&amp;id=2d26c787b7" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
         <div id="mc_embed_signup_scroll" class="form-container">
           <div class="name-field field">
             <input type="text" value="" placeholder="First Name" name="FNAME" class="required" id="mce-FNAME">
@@ -32,16 +32,16 @@
             <div class="response" id="mce-error-response" style="display:none"></div>
             <div class="response" id="mce-success-response" style="display:none"></div>
           </div>
-          <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-          <div style="position: absolute; left: -5000px;" aria-hidden="true">
-            <input type="text" name="b_f07999ddf32b42be0af661143_9fe85973c9" tabindex="-1" value="">
-          </div>
+          <div class="response" id="mce-success-response" style="display:none"></div>
+	</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+          <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_e6ab7586e5e3358172da22416_2d26c787b7" tabindex="-1" value=""></div>
           <div class="field submit">
             <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button">
           </div>
         </div>
       </form>
     </div>
+    <script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';fnames[3]='ADDRESS';ftypes[3]='address';fnames[4]='PHONE';ftypes[4]='phone';fnames[5]='BIRTHDAY';ftypes[5]='birthday';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
   </div>
 </footer>
   <div class="colophon">
