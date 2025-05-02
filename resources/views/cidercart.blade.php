3a05dccb22f5f24f29e78ef46046047dda4f5590
Patrick Kerby <patrick@kerbydesign.com>
2025-05-02 10:50:33 -0600

    Add cidercart styles

--- resources/views/cidercart.blade.php
+++ resources/views/cidercart.blade.php
@@ -13,22 +13,19 @@
 
 @endphp
 
-@extends('layouts.app')
+@extends('layouts.cidercart')
 
 @section('content')
   @while(have_posts()) @php the_post() @endphp
     {{-- @include('partials.page-header') --}}
-    <div id="hero">
-      <h2>
+      <h1 class="hidden">
           {{ $title }}
-      </h2>
-      <img class="full-bg" src="{{ $image }}" />
-    </div>
-    <section class="intro">
+      </h1>
+    <section class="landing-page">
         @include('partials.content-page')
     </section>
     
-    <section class="social">
+    {{-- <section class="social">
       <h3>Follow along</h3>
       <p>Our journey has just begun and we still have a lot to do! Follow us for product and orchard updates on Instagram and Facebook.</p>
       <ul class="social-buttons">
@@ -36,18 +33,7 @@
         <li><a href="https://www.facebook.com/profile.php?id=100060722582049" target="_blank"><img src="@asset('images/instagram.svg')" /></a></li>
       </ul>
       @php dynamic_sidebar('sidebar-footer') @endphp
-    </section>
-    <section class="contact row">
-      <div class="col-half col1">
-        <h2>Look around.</h2>
-        <h2>Try our cider.</h2>
-        <h2>Send us a message!</h2>
-      </div>
-      <div class="col-half col2">
-        <h4>Where you can find us</h4>
-        <p>Find us at markets around Edmonton (<a href="/events/">check out our market schedule</a>) or order online. Follow us on facebook or instagram for more info!</p>
-        @php gravity_form( 1, false, false, false, '', true, 12 );@endphp
-      </div>
-    </section>
+    </section> --}}
   @endwhile
 @endsection
+
---
+++ resources/views/layouts/cidercart.blade.php
@@ -0,0 +1,65 @@
+<!doctype html>
+<html {!! get_language_attributes() !!}>
+  @include('partials.head')
+  <body @php body_class() @endphp>
+
+    @php do_action('get_header') @endphp
+    
+    {{-- Mobile Navigation --}}
+    <nav class="nav-mobile">
+      <input class="side-menu" type="checkbox" id="side-menu"/>
+      <label class="hamb" for="side-menu" @if(is_front_page())style="background-color: var(--color-scheme);"@endif">
+        <span class="hamb-line"></span>
+        <span class="nav-title">Menu</span>
+      </label>
+      <nav class="side-nav col-md" role="navigation">
+        @if (has_nav_menu('primary_navigation'))
+          {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
+        @endif
+      </nav>
+    </nav>
+
+    {{-- Desktop Navigation --}}
+    <nav class="nav-desktop" role="navigation">
+      @if (has_nav_menu('primary_navigation'))
+        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav-top']) !!}
+      @endif
+    </nav>
+
+    {{-- @include('partials.header') --}}
+    <div class="wrap container" role="document">
+      @yield('content')
+
+      <div id="cidercart-contact" class="modalDialog">
+        <div class="stockist-container">
+          <a href="#close" title="Close" class="close">Close</a>
+          <h2>Book The Cider Cart!</h2>
+          <p>Send us a message with all of your details, we'll get back to you as soon as possible!</p>
+          <div class="stockist-content">
+            @php gravity_form( 1, false, false, false, '', true, 12 );@endphp
+          </div>
+        </div>
+      </div>
+
+      <div id="stockists" class="modalDialog">
+        <div class="stockist-container">
+          <a href="#close" title="Close" class="close">Close</a>
+          <h2>Stockists</h2>
+          <p>We're slowly but surely getting our cider into the best bottle shops around! If you'd like your local to carry us, let them know and ask them to get in touch!</p>
+          <div class="stockist-content">
+            <p>{!! $acf_options->stockists !!}</p>
+          </div>
+        </div>
+      </div>
+
+    </div>
+
+    @php do_action('get_footer') @endphp
+    @include('partials.footer')
+    @php wp_footer() @endphp
+    <a href="#" class="close-product" onclick="return false;">close</a>
+
+   
+
+  </body>
+</html>
