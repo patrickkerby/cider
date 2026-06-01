eba63730954d1e8f13549fba20fae3260c24c130
Patrick Kerby <patrick@kerbydesign.com>
2026-06-01 17:51:05 -0600

    update delivery verbiage

--- resources/woocommerce-quick-view/quick-view.php
+++ resources/woocommerce-quick-view/quick-view.php
@@ -59,7 +59,7 @@ do_action( 'wc_quick_view_before_single_product' );
 				<strong>Camrose &amp; Red Deer</strong>: June 14, 10am - 6pm<br>
 				<strong>Edmonton:</strong> June 18 &amp; 19, 10am - 1pm<br><br>
 
-				<strong>Regular Delivery</strong><br>
+				<strong>Regular Edmonton Delivery</strong><br>
 				Order online and get cider delivered to your door!<br><br>
 				Contact us to arrange a delivery in the Edmonton area. The purchaser must be home to accept the delivery and show proof of age.<br><br>
 				If you live outside the Edmonton area, just give us a shout and we'll see if we can arrange something that works.
